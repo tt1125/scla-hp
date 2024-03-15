bcf87264b0caa872d7a6af67d74f1d5c16438de6
tt1125 <tsurutsuru275@gmail.com>
2024-03-15 22:07:12 +0900

    ic

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Header } from "./components/Header";
+
+function App() {
+  return (
+    <main>
+      {/* <Header /> */}
+      <div style={{ display: "flex", height: "100vh", justifyContent: "center", alignItems: "center" }}>
+         <p style={{ fontSize: "80px" }}>コンピュータ研究会</p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
